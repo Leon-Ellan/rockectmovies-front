20a73e46605444a784609e30105055b574a2b40e
Leon Martins <leonellan12@gmail.com>
2023-11-24 09:51:26 -0300

    add user image

--- rocketmovies/src/components/Header/index.jsx
+++ rocketmovies/src/components/Header/index.jsx
@@ -1,13 +1,18 @@
-import { Container } from "./styles";
+import { Container, Profile } from "./styles";
 
 export function Header() {
 
   return (
     <Container>
+      <Profile>
+        <img 
+        src="https://github.com/leon-ellan.png" 
+        alt="foto do usuário" />
       <div>
         <span>Bem-vindo</span>
-        <strong>Name</strong>
+        <strong>Leon Ellan</strong>
       </div>
+      </Profile>
     </Container>
   )
 }
