c75f7d2937c705d3d4a1cc211fb52faf4fe65995
Leon Martins <leonellan12@gmail.com>
2023-11-24 19:29:06 -0300

    add Tag component

--- rocketmovies/src/pages/Details/index.jsx
+++ rocketmovies/src/pages/Details/index.jsx
@@ -2,14 +2,14 @@ import { Button } from "../../components/Button";
 import { Header } from "../../components/Header";
 import { Container, Links } from "./styles";
 import { Section } from "../../components/Section";
+import { Tag } from "../../components/Tags";
 
 export function Details() {
 
   return (
     <Container>
-      <Header/>
-      <Button title="Voltar"/>
-      <Section title="Links úteis"> 
+     <Header/>
+       <Section title="Links úteis"> 
         <Links>
          
            <li><a href="#" target="_blank">link.1</a>
@@ -18,10 +18,13 @@ export function Details() {
            </li>
           
         </Links>
-      </Section>
+       </Section>
       <Section title="Marcadores"> 
-      
+        <Tag title="Ação"/>
+        <Tag title="Aventura"/>
+
       </Section>
+      <Button title="Voltar"/>
     </Container>   
 
   )
